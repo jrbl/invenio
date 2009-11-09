46deff8baf2f6cf1e91842184e597cc375be6b71
Joe Blaylock <jrbl@slac.stanford.edu>
2009-11-09 15:16:25 -0800

    editauthor: gathering affils per auth by recID
    Now it's time for some actual UI coding.

--- modules/bibedit/lib/editauthor_webinterface.py
+++ modules/bibedit/lib/editauthor_webinterface.py
@@ -3,6 +3,7 @@ import invenio.webpage
 import invenio.template
 from invenio.webinterface_handler import WebInterfaceDirectory, wash_urlargd
 
+import invenio.editauthor_engine as engine
 
 class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
     """Handle URLs is the /editauthors tree"""
@@ -31,8 +32,20 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
 
         if not form.has_key('recID') or form['recID'] == -1:
             return self.index(request, {})
-        else:
-            text = self.template.record(form)
+
+        record_id = form['recID']
+
+        authors = engine.recid2names(record_id)
+        affils = {}
+        for author in authors:
+            for id, name, inst in engine.name2affils(author, record_id):
+                if affil != None:
+                    if not affils.has_key(author):
+                        affils[author] = [inst]
+                    else:
+                        affils[author].append(inst)
+
+        text = self.template.record(record_id, affils)
 
         return invenio.webpage.page(title = self.title,
                                     body  = text,
