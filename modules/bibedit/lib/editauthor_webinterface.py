ae1f0815ae25aa4e595e560ae9da4c55f02a707b
Joe Blaylock <jrbl@slac.stanford.edu>
2010-01-08 14:51:22 -0800

    editauthor: changes to pass kwalitee checks

--- modules/bibedit/lib/editauthor_webinterface.py
+++ modules/bibedit/lib/editauthor_webinterface.py
@@ -11,6 +11,7 @@ import invenio.access_control_engine as webuser_access
 import invenio.bibtask as bibtask
 import invenio.bibknowledge as bibknowledge
 
+
 class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
     """Handle URLs is the /editauthors tree"""
 
@@ -23,7 +24,7 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
         self.columns_to_show = 10
 
     def index(self, request, form):
-        
+
         permission = check_request_allowed(request)
         if permission != True:
             return permission
@@ -36,8 +37,8 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
             return self.rec(request, f)
 
         return invenio.webpage.page(title = self.title,
-                                    body  = self.template.index(),
-                                    req   = request,)
+                                    body = self.template.index(),
+                                    req = request)
 
     def rec(self, request, form):
 
@@ -45,7 +46,8 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
         if permission != True:
             return permission
 
-        if not form.has_key('recID') or form['recID'] == -1:
+        #if not form.has_key('recID') or form['recID'] == -1:
+        if not 'recID' in form or form['recID'] == -1:
             return self.index(request, {})
 
         record_id = form['recID']
@@ -60,24 +62,34 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
         place_list = engine.flattenByCounts(place_list)
 
         # FIXME: hardcoded (& incorrect) KB name
-        validated_affiliations = [x[0] for x in bibknowledge.get_kbr_keys("JoeTest") if x[0] != '']
+        validated_affiliations = [x[0] for x in
+                                  bibknowledge.get_kbr_keys("JoeTest") if
+                                  x[0] != '']
 
-        text = self.template.record(record_id, author_list, place_list, validated_affiliations)
+        text = self.template.record(record_id,
+                                    author_list,
+                                    place_list,
+                                    validated_affiliations)
 
         return invenio.webpage.page(title = self.title,
-                                    body  = text,
-                                    req   = request,)
+                                    body = text,
+                                    req = request)
 
     def checkAffil(self, request, form):
-        if not form.has_key('affil'):
-            return self.index(request, {} )
+        #if not form.has_key('affil'):
+        if not 'affil' in form:
+            return self.index(request, {})
 
-        form_data = wash_urlargd(form, {'affil': (str, '')} )
+        form_data = wash_urlargd(form, {'affil': (str, '')})
 
         possible_matches = []
-        possible_matches = [x[0] for x in invenio.bibknowledge.get_kbr_values('JoeTest', form_data['affil'], 'e') if x[0] != '']
+        possible_matches = [x[0] for x in
+                            invenio.bibknowledge.get_kbr_values(
+                                    'JoeTest',
+                                    form_data['affil'],
+                                    'e') if x[0] != '']
         return simplejson.dumps(possible_matches)
-        #return invenio.webpage.page(title = '', body = form_data, req = request)
+        #return invenio.webpage.page(title='', body=form_data, req=request
 
     def process(self, request, form):
 
@@ -106,37 +118,46 @@ class WebInterfaceEditAuthorPages(WebInterfaceDirectory):
             return washer
 
         form_data = wash_urlargd(form, get_washer(form))
-        form_data = utils.json_unicode_to_utf8(form_data)            # clean up unicode entitites
-        form_length = (len(form_data) - 2) / 2   # minus 'ln' key and 'recid' key, / paired items
+            # clean up unicode entitites
+        form_data = utils.json_unicode_to_utf8(form_data)
+            # minus 'ln' key and 'recid' key, / paired items
+        form_length = (len(form_data) - 2) / 2
 
         new_doc = '<?xml version="1.0" encoding="UTF-8"?>\n'
         new_doc += '<collection xmlns="http://www.loc.gov/MARC21/slim">\n'
-        new_doc += "<record>\n  <controlfield tag=\"001\">%s</controlfield>\n" % form_data['recID']
+        new_doc += "<record>\n  <controlfield tag=\"001\">"
+        new_doc += "%s</controlfield>\n" % form_data['recID']
         new_doc += "  <datafield tag=\"100\" ind1=\" \" ind2=\" \">\n"
-        new_doc += "    <subfield code=\"a\">%s</subfield>\n" % form_data['autho0']
+        new_doc += "    <subfield code=\"a\">"
+        new_doc += "%s</subfield>\n" % form_data['autho0']
         for sub in form_data['insts0'].split(';'):
             new_doc += "    <subfield code=\"u\">%s</subfield>\n" % sub
         new_doc += "  </datafield>\n"
-        for i in range(1,form_length):
+        for i in range(1, form_length):
             new_doc += "  <datafield tag=\"700\" ind1=\" \" ind2=\" \">\n"
-            new_doc += "    <subfield code=\"a\">%s</subfield>\n" % form_data['autho%s'%i]
+            new_doc += "    <subfield code=\"a\">"
+            new_doc += "%s</subfield>\n" % form_data['autho%s'%i]
             for sub in form_data['insts%s'%i].split(';'):
                 new_doc += "    <subfield code=\"u\">%s</subfield>\n" % sub
             new_doc += "  </datafield>\n"
         new_doc += "</record>\n</collection>"
 
-        # We can't use utils.save_xml_record because this isn't a complete record
-        newdoc_filename = utils._get_file_path(form_data['recID'], webuser.getUid(request))
+        # Don't use utils.save_xml_record because record isn't complete
+        newdoc_filename = utils._get_file_path(form_data['recID'],
+                                               webuser.getUid(request))
         newdoc_file = open(newdoc_filename, 'w')
         newdoc_file.write(new_doc)
         newdoc_file.close()
-        bibtask.task_low_level_submission('bibupload', 'bibedit', '-P', '5', '-c', newdoc_filename)
-        
+        bibtask.task_low_level_submission('bibupload', 'bibedit', '-P',
+                                          '5', '-c', newdoc_filename)
+
         ret_title = "editauthors: Record %s submitted" % form_data['recID']
-        ret_body = "The following record modification has been submitted for record id %s:\n%s\n" % (form_data['recID'], debugPrint(form_data))
+        ret_body = "The following record modification has been submitted for "
+        ret_body += "record id %s:\n%s\n" % (form_data['recID'],
+                                             debugPrint(form_data))
         return invenio.webpage.page(title = ret_title,
-                                    body  = ret_body,
-                                    req   = request,)
+                                    body = ret_body,
+                                    req = request)
 
     def __call__(self, request, form):
         return self.index(request, form)
@@ -146,6 +167,8 @@ def check_request_allowed(request):
     """Is this request allowed to this user?  Return True or a failure page."""
     code, message = webuser_access.acc_authorize_action(request, 'runbibedit')
     if code != 0:
-        return webuser.page_not_authorized(req = request, referer = '/editauthors', 
+        return webuser.page_not_authorized(req = request,
+                                           referer = '/editauthors',
                                            text = message)
-    else: return True
+    else:
+        return True
