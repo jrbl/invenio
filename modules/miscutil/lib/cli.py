fce31443973ad2c266667408143e89d98a951247
Travis Brooks <travis@slac.stanford.edu>
2010-10-06 11:30:22 -0700

    cli: initial commit of cli.py

---
+++ modules/miscutil/lib/cli.py
@@ -0,0 +1,8 @@
+import invenio
+from invenio.search_engine import perform_request_search
+from invenio.search_engine import get_record
+from invenio.bibrank_citation_searcher import get_citation_dict
+from invenio.bibformat import format_record
+from invenio.bibformat import format_records
+from invenio.intbitset import intbitset
+
